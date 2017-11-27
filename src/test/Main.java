d77beb6e9e4ef3f587f7ad4bf879caa7e6fc9e6b
osaifh <raimon_to@hotmail.com>
2017-11-27 17:24:40 +0100

    Afegit input dels jugadors

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,9 +1,21 @@
 package test;
 
+import java.util.Scanner;
+import model.Board;
 public class Main {
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner user_input = new Scanner(System.in);
+        int x, y;
+        Board board = new Board();
+        while (true){
+            do {
+                System.out.println("Introdueix x");
+                x = user_input.nextInt();
+                System.out.println("Introdueix y");
+                y = user_input.nextInt();
+            } while (!board.input(x, y));
+            board.print();
+        }
     }
-    
 }
