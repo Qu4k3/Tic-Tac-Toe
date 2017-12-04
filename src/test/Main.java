56463c5f50a70e36ea2fd4a8f6bc90abbcbea59f
Qu4k3 <paul.guillamon@gmail.com>
2017-12-04 20:06:50 +0100

    torn jugadors

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,4 +1,4 @@
- package test;
+package test;
 
 import java.util.Scanner;
 import jdk.nashorn.internal.objects.NativeString;
@@ -11,7 +11,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner user_input = new Scanner(System.in);
         boolean running = true;
-        int x, y;
+        int x, y, auxTurn = 0;
         String p1_name, p2_name;
         char p1_ficha, p2_ficha;
 
@@ -41,42 +41,50 @@ public class Main {
         p2_name = user_input.next();
         Player p2 = new Player(p2_name, p2_ficha);
 
-        System.out.println("\n" + ANSI_YELLOW + p1.getName() + ANSI_RESET + " comença:");
+        Board board = new Board(p1, p2);
 
-        Board board = new Board(p1,p2);
-
-        while (running){
+        while (running) {
             do {
-                
+
+                if ((auxTurn % 2) == 0) {
+                    System.out.println("\nÉs el torn de: " + ANSI_YELLOW + p1.getName() + ANSI_RESET);
+                } else {
+                    System.out.println("\nÉs el torn de " + ANSI_YELLOW + p2.getName() + ANSI_RESET);
+                }
+
+                auxTurn++;
+
                 System.out.println("\n" + ANSI_CYAN + "// Introdueix posició x" + ANSI_RESET);
-                System.out.println("\n" 
+                System.out.println("\n"
                         + ANSI_YELLOW + "|0|1|2|\n"
                         + ANSI_YELLOW + "|0|1|2|\n"
-                        + ANSI_YELLOW + "|0|1|2|\n");
+                        + ANSI_YELLOW + "|0|1|2|\n" + ANSI_RESET);
                 System.out.print("> ");
                 x = user_input.nextInt();
-                
-                System.out.println("\n" + ANSI_CYAN + "// Introdueix posició y" +ANSI_RESET);
-                System.out.println("\n" 
+
+                System.out.println("\n" + ANSI_CYAN + "// Introdueix posició y" + ANSI_RESET);
+                System.out.println("\n"
                         + ANSI_YELLOW + "|0|0|0|\n"
                         + ANSI_YELLOW + "|1|1|1|\n"
-                        + ANSI_YELLOW + "|2|2|2|\n");
+                        + ANSI_YELLOW + "|2|2|2|\n" + ANSI_RESET);
                 System.out.print("> ");
                 y = user_input.nextInt();
-                
+
             } while (!board.input(x, y));
-            
-            System.out.println("\nTablero: \n");
+
+            System.out.println("\n" + ANSI_GREEN + "// Estat del taulell" + ANSI_RESET + "\n");
             board.print();
-            
-            running = (board.victoryCondition()==' ');
-            
+
+            running = (board.victoryCondition() == ' ');
+
         }
-        
-        if (board.victoryCondition()==p1.getFicha()) System.out.println("\n" + ANSI_PURPLE_BACKGROUND + ANSI_WHITE + " El jugador " + p1.getName()  + " guanya " + ANSI_RESET);
-        
-        else if (board.victoryCondition()==p2.getFicha()) System.out.println("\n" + ANSI_PURPLE_BACKGROUND + ANSI_WHITE + " El jugador " + p2.getName() + " guanya " + ANSI_RESET);
-        
-        else System.out.println("\n" + ANSI_PURPLE_BACKGROUND + "Empat" + ANSI_RESET);
+
+        if (board.victoryCondition() == p1.getFicha()) {
+            System.out.println("\n" + ANSI_PURPLE_BACKGROUND + ANSI_WHITE + " El jugador " + p1.getName() + " guanya " + ANSI_RESET);
+        } else if (board.victoryCondition() == p2.getFicha()) {
+            System.out.println("\n" + ANSI_PURPLE_BACKGROUND + ANSI_WHITE + " El jugador " + p2.getName() + " guanya " + ANSI_RESET);
+        } else {
+            System.out.println("\n" + ANSI_PURPLE_BACKGROUND + "Empat" + ANSI_RESET);
+        }
     }
 }
